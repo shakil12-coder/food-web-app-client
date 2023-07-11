eab09409027b7ff63d0710ae0d28f789a8816f5f
Shakil Ahmed <shakilahmed0888@gmail.com>
2023-07-11 23:34:28 +0530

    initial commite

---
+++ src/screens/Cart.js
@@ -0,0 +1,85 @@
+import React from "react";
+// import trash from '../trash.svg'
+import { useCart, useDispatchCart } from "../components/ContextReducer";
+function Cart() {
+  let data = useCart();
+  let dispatch = useDispatchCart();
+  if (!data || data.length === 0) {
+    return (
+      <div>
+        <div className="m-5 w-100 text-center fs-3">The Cart is Empty!</div>
+      </div>
+    );
+  }
+  const handleCheckOut = async () => {
+    let userEmail = localStorage.getItem("userEmail");
+    console.log("userEmail : ", userEmail);
+    let response = await fetch("http://localhost:5000/api/orderData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order_data: data,
+        email: userEmail,
+        order_date: new Date().toDateString(),
+      }),
+    });
+    console.log("order response :", response);
+    if (response.status === 200) {
+      dispatch({ type: "DROP" });
+    }
+  };
+  let totalPrice = data.reduce((total, food) => total + food.price, 0);
+  return (
+    <div>
+      <div className="container m-auto mt-5 table-responsive table-responsive-sm table-responsive-md table-responsive-lg">
+        <table className="table table-hover">
+          <thead className="text-success fs-4">
+            <tr>
+              <th scope>#</th>
+              <th scope>Name</th>
+              <th scope>Quantity</th>
+              <th scope>Option</th>
+              <th scope>Amount</th>
+              <th scope></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((food, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{food.name}</td>
+                <td>{food.qty}</td>
+                <td>{food.size}</td>
+                <td>{food.price}</td>
+                <td>
+                  <button type="button" className="btn p-0">
+                    <img
+                      src={""}
+                      alt="delete"
+                      onClick={() => {
+                        dispatch({ type: "REMOVE", index: index });
+                      }}
+                    />
+                  </button>{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <h1 className="fs-2">Total Price: {totalPrice}/-</h1>
+        </div>
+        <div>
+          <button className="btn bg-success mt-5 " onClick={handleCheckOut}>
+            {" "}
+            Check Out{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/screens/MyOrder.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function MyOrder() {
+  const [orderData, setOrderData] = useState({});
+  //   console.log(localStorage.getItem("userEmail"));
+
+  const fetchMyOrder = async () => {
+    // console.log(localStorage.getItem("userEmail"));
+    await fetch("http://localhost:5000/api/myOrderData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: localStorage.getItem("userEmail"),
+      }),
+    }).then(async (res) => {
+      let response = await res.json();
+      console.log(response);
+      setOrderData(response);
+      console.log("data is : ", orderData);
+    });
+  };
+
+  useEffect(() => {
+    fetchMyOrder();
+  });
+
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+
+      <div className="container">
+        <div className="">
+          {orderData !== {}
+            ? Array(orderData).map((data) => {
+                return data.orderData
+                  ? data.orderData.order_data
+                      .slice(0)
+                      .reverse()
+                      .map((item) => {
+                        return item.map((arrayData) => {
+                          return (
+                            <>
+                              <div className="">
+                                {arrayData.order_date ? (
+                                  <div className="m-auto mt-5">
+                                    {(data = arrayData.order_date)}
+                                    <hr />
+                                  </div>
+                                ) : (
+                                  <div className="">
+                                    <div
+                                      className="card mt-3"
+                                      style={{
+                                        width: "16rem",
+                                        maxHeight: "360px",
+                                      }}
+                                    >
+                                      <div className="d-flex align-items-center justify-content-center">
+                                        <div className="card-body">
+                                          <h5 className="card-title">
+                                            {arrayData.name}
+                                          </h5>
+                                          <div
+                                            className="container w-100 p-0"
+                                            style={{ height: "38px" }}
+                                          >
+                                            <span className="m-1">
+                                              {arrayData.qty}
+                                            </span>
+                                            <span className="m-1">
+                                              {arrayData.size}
+                                            </span>
+                                            <br />
+
+                                            <span className="m-1">{data}</span>
+                                            <br />
+                                          </div>
+                                        </div>
+                                        <div
+                                          style={{
+                                            border: "solid",
+                                            borderColor: "white",
+                                            borderWidth: "0.5px",
+                                            height: "80px",
+                                          }}
+                                        ></div>
+                                        <div className=" d-inline ms-2 h-100 w-20 fs-5">
+                                          ₹{arrayData.price}/-
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                )}
+                              </div>
+                            </>
+                          );
+                        });
+                      })
+                  : "";
+              })
+            : ""}
+        </div>
+      </div>
+
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default MyOrder;
